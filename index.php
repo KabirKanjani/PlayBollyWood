c806ab9e6bdb30e881ed79b0617436a91db701fa
Kabir Kanjani <andrewkabir@gmail.com>
2022-01-01 12:59:56 +0530

    first commit

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 session_start();
 if(isset($_POST['checker'])&&$_POST['checker']==12)
 {
-  echo $_POST['checker'];
   $_POST['checker']=0;
-  header('Location:https://playbollywood.herokuapp.com/');
+  header('Location:https://playbollywood.herokuapp.com/?mark=0&Start=Start');
 
 }
 ?>
@@ -190,13 +189,13 @@ if(isset($_POST['checker'])&&$_POST['checker']==12)
     {
       if(c==0)
       {
-        document.POSTElementById("box").style.display="block";
+        document.getElementById("box").style.display="block";
         c=1;
       }
       else {
-        document.POSTElementById("box").style.display="none";
+        document.getElementById("box").style.display="none";
         c=0;
-        window.location.href="http://localhost/files/git/bollywood/https://playbollywood.herokuapp.com/?mark=0&Start=Start&";
+        window.location.href="https://playbollywood.herokuapp.com/?mark=0&Start=Start&";
           }
     }
   </script>
@@ -208,307 +207,311 @@ if(isset($_POST['checker'])&&$_POST['checker']==12)
     <h1>PLAY BOLLYWOOD</h1>
   </div>
 <input type="hidden" name="OldLetters" value="">
-<form action="https://playbollywood.herokuapp.com/" method="POST" id="bw" name="bollywood" >
+<form action="index.php" method="POST" id="bw" name="bollywood" >
 <input type="hidden" name="mark" value=0>
 
 <div class="content">
-<?php
-if(!isset($_POST['checker']))
-{
-  echo "<input type='hidden' name='checker'>";
-}
-  if(isset($_POST['change']))
-  {
-              $_SESSION['count']=0;
-              session_destroy();
-              session_unset();
-              session_start();
-  }
 
-  if(isset($_POST['end']))
-{
-  if($_POST['end']==0)
-  {
-      header('Location:https://playbollywood.herokuapp.com/');
-  }
-}
-if(!isset($_SESSION['oldLetters']))
-$_SESSION['oldLetters']="";
-if(!isset($_SESSION['count']))
-{
-  $_SESSION['count']=9;
-}
-function POSTmovie()
-{
-          if(!isset($_SESSION['movie']))
-          {
-                        $connect=mysqli_connect("us-cdbr-east-04.cleardb.com", "b5a264c5c9d4c6","b8abe23f","bollywood");      //$db=mysqli_select_db($connect,"bollywood");
-                        //$db=mysqli_select_db($connect,"bollywood");
-                        $query="SELECT * from bollywood";
-                        $query_run=mysqli_query($connect,$query);
-                        $num_rows=mysqli_num_rows($query_run);
-                        $n=rand(0,$num_rows);
+  <?php
 
-                        $query="SELECT Mname from bollywood where Mid=$n";
-                        $query_run=mysqli_query($connect,$query);
-                        $data=mysqli_fetch_assoc($query_run);
-                        if(is_null($data['Mname']))
-                        POSTmovie();
-
-                        $data=preg_replace('/[^ \w]+/', '', $data['Mname']);
-                        return $data;
-        }
-}
-
-function oldletter()
-{
-  $ol="";
-  if(isset($_POST['LetterGuessed']))
-  {
-
-      if(isset($_SESSION['oldLetters'])&&!str_contains($_SESSION['oldLetters'],$_POST['LetterGuessed']))
-      {
-        $_SESSION['movie']=strtoupper($_SESSION['movie']);
-        $_SESSION['oldLetters']=$_SESSION['oldLetters']." ".$_POST['LetterGuessed'];
-        $ol=$_SESSION['oldLetters'];
-      }
-      else if(isset($_SESSION['oldLetters']))
-      {
-        $ol=$_SESSION['oldLetters'];
-      }
-  }
-  echo "<div class='ol'><font size=18>Your Old Letters are";
-  if(isset($_SESSION['oldLetters']))
-  {
-    echo $ol."<br></font></div>";
-  }
-}
-function setmovie()
-{
-  if(!isset($_SESSION['movie']))
+    if(isset($_POST['change']))
     {
-      $_SESSION['movie']=POSTmovie();
+                $_SESSION['count']=0;
+                session_destroy();
+                session_unset();
     }
-}
-function guessmovie()
-{
-  $guess="";
-  if(isset($_SESSION['movie']))
+  if(!isset($_POST['streak']))
   {
-      $movie=strtoupper($_SESSION['movie']);
-          for($i=0;$i<strlen($movie);$i++)
-            {
-                      if($movie[$i]==' ')
-                      {
-                        $guess[$i]='/';
-                      }
-                      else if($movie[$i]=='A'||$movie[$i]=='E'||$movie[$i]=='I'||$movie[$i]=='O'||$movie[$i]=='U')
-                      {
-                        $guess[$i]=strtoupper($movie[$i]);
-                      }
-                      else
-                      {
-                      $guess[$i]='_';
-                      }
-            }
-      if(!isset($_SESSION['guess1']))
-        {
-        $_SESSION['guess1']=$guess;
-       }
+    $_POST['streak']=0;
   }
-}
-function checkwinlose()
-{
-  echo "<div class=result><font size=18>";
-  if(isset($_SESSION['guess1'])&&!str_contains($_SESSION['guess1'],'_'))
+    if(isset($_POST['end']))
   {
-    $_POST['checker']=12;
-    echo $_POST['checker'];
-    unset($_POST['$LetterGuessed']);
-    echo "You Win";
+    if($_POST['end']==0)
+    {
+        //header('Location:http://localhost/files/git/uniquerandom.php?mark=0&Start=Start');
+		
+    }
+  }
+  if(!isset($_SESSION['oldLetters']))
+  $_SESSION['oldLetters']="";
+  if(!isset($_SESSION['count']))
+  {
     $_SESSION['count']=9;
-      echo"
-              <div id='box'>
-             <span class='ion-checkmark-round'>
-                <h1>You Win</h1>
-                <h5>Movie : ".$_SESSION['movie']."</h5>
-                </span>
-                <a class='close' onclick='pop()'>close</a>
-        </div>
-";
-      echo "<SCRIPT LANGUAGE='javascript'>pop();</SCRIPT>";
-      session_unset();
-      session_destroy();
-      echo "<form name='check' method=POST><input type='hidden' name='end' value='0'></form>";
-    }
-    if(isset($_SESSION['count'])&&$_SESSION['count']==0)
-    {
-      $_POST['checker']=12;
-      unset($_POST['$LetterGuessed']);
-      echo "You Lose"."<br>";
-      $_SESSION['count']=9;
-      echo"
-              <div id='box'>
-             <span class='ion-checkmark-round'>
-                <h1>You Lose</h1>
-                <h5>Movie:".$_SESSION['movie']."</h5>
-                </span>
-                <a class='close' onclick='pop()'>close</a>
-        </div>
-";      echo "<SCRIPT LANGUAGE='javascript'>pop();</SCRIPT>";
+  }
+  function GETmovie()
+  {
+            if(!isset($_SESSION['movie']))
+            {
+                          $connect=mysqli_connect("us-cdbr-east-05.cleardb.net","b1a142f1bac86d","7f2c1843","heroku_711e99c5734cbf2");
+                          //$db=mysqli_select_db($connect,"bollywood");
+                          $query="SELECT * from bollywood";
+                          $query_run=mysqli_query($connect,$query);
+                          $num_rows=mysqli_num_rows($query_run);
+                          $n=rand(0,$num_rows);
 
-      echo $_SESSION['movie'];
-      if(isset($_SESSION))
+                          $query="SELECT Mname from bollywood where Mid=$n";
+                          $query_run=mysqli_query($connect,$query);
+                          $data=mysqli_fetch_assoc($query_run);
+                          if(is_null($data['Mname']))
+                          GETmovie();
+
+                          $data=preg_replace('/[^ \w]+/', '', $data['Mname']);
+                          return $data;
+          }
+  }
+
+  function oldletter()
+  {
+    $ol="";
+    if(isset($_POST['LetterGuessed']))
+    {
+
+        if(isset($_SESSION['oldLetters'])&&!str_contains($_SESSION['oldLetters'],$_POST['LetterGuessed']))
+        {
+          $_SESSION['movie']=strtoupper($_SESSION['movie']);
+          $_SESSION['oldLetters']=$_SESSION['oldLetters']." ".$_POST['LetterGuessed'];
+          $ol=$_SESSION['oldLetters'];
+        }
+        else if(isset($_SESSION['oldLetters']))
+        {
+          $ol=$_SESSION['oldLetters'];
+        }
+    }
+    echo "<div class='ol'><font size=18>Your Old Letters are";
+    if(isset($_SESSION['oldLetters']))
+    {
+      echo $ol."<br></font></div>";
+    }
+  }
+  function setmovie()
+  {
+    if(!isset($_SESSION['movie']))
       {
+        $_SESSION['movie']=GETmovie();
+      }
+  }
+  function guessmovie()
+  {
+    $guess="";
+    if(isset($_SESSION['movie']))
+    {
+        $movie=strtoupper($_SESSION['movie']);
+            for($i=0;$i<strlen($movie);$i++)
+              {
+                        if($movie[$i]==' ')
+                        {
+                          $guess[$i]='/';
+                        }
+                        else if($movie[$i]=='A'||$movie[$i]=='E'||$movie[$i]=='I'||$movie[$i]=='O'||$movie[$i]=='U')
+                        {
+                          $guess[$i]=strtoupper($movie[$i]);
+                        }
+                        else
+                        {
+                        $guess[$i]='_';
+                        }
+              }
+        if(!isset($_SESSION['guess1']))
+          {
+          $_SESSION['guess1']=$guess;
+         }
+    }
+  }
+  function checkwinlose()
+  {
+    echo "<div class=result><font size=18>";
+    if(isset($_SESSION['guess1'])&&!str_contains($_SESSION['guess1'],'_'))
+    {
+
+      unset($_POST['$LetterGuessed']);
+      echo "You Win";
+      $_SESSION['count']=9;
+        echo"
+                <div id='box'>
+               <span class='ion-checkmark-round'>
+                  <h1>You Win</h1>
+                  <h5>Movie : ".$_SESSION['movie']."</h5>
+                  </span>
+                  <a class='close' onclick='pop()'>close</a>
+          </div>
+  ";
+        echo "<SCRIPT LANGUAGE='javascript'>pop();</SCRIPT>";
         session_unset();
         session_destroy();
+        echo "<form name='check' method=POST><input type='hidden' name='end' value='0'></form>";
+          $_POST['streak']++;
       }
-      echo "<form name='check' method=POST><input type='hidden' name='end' value='0'></form>";
+      if(isset($_SESSION['count'])&&$_SESSION['count']==0)
+      {
+        echo "You Lose"."<br>";
+        unset($_POST['$LetterGuessed']);
+        $_SESSION['count']=9;
+        echo"
+                <div id='box'>
+               <span class='ion-checkmark-round'>
+                  <h1>You Lose</h1>
+                  <h5>Movie:".$_SESSION['movie']."</h5>
+                  </span>
+                  <a class='close' onclick='pop()'>close</a>
+          </div>
+  ";      echo "<SCRIPT LANGUAGE='javascript'>pop();</SCRIPT>";
 
-    }
-      $_POST['LetterGuessed'] = array();
-      echo "</div></font>";
-}
-function checkguessmovie($x)
-{
-  if(isset($_POST['LetterGuessed']))
-  {
-      oldLetter();
-  }
-            $mark=1;
-            if(isset($_SESSION['guess1']))
-            {
-              echo "<div class='guess'>";
-                  for($i=0;$i<strlen($_SESSION['guess1']);$i++)
-                {
-                    if(isset($_POST['LetterGuessed']))
-                      {
-                      $LetterGuessed=$_POST['LetterGuessed'];
-                          if($_SESSION['movie'][$i]==$LetterGuessed)
-                            {
-                                $_SESSION['guess1'][$i]=$LetterGuessed;
-                                $mark=0;
-                            }
-                      }
-                  echo $_SESSION['guess1'][$i]." ";
-              }
-               echo "</div></font>";
-            }
-            if($x==0)
-            {
-                if($mark==1&&isset($_SESSION['count']))
-                {
-                $_SESSION['count']--;
-                }
-            }
-}
-function bolly()
-{
-
-  $text="BOLLYWOOD";
-  echo "<br>";
-  echo "<div class='bolly'>";
-  // if(isset($_SESSION['count']))
-  for($i=0;$i<9-$_SESSION['count'];$i++)
-  {
-    echo $text[$i];
-  }
-  echo "</div>";
-}
-
-function letters()
-{
-  echo "<div id=letters>";
-  for($x=0;$x<26;$x++)
-  {
-        if($x%10==0)
+        echo $_SESSION['movie'];
+        if(isset($_SESSION))
         {
-          echo"<br><br>";
+          session_unset();
+          session_destroy();
         }
-        $letter=chr($x+65);
-        echo '<input type="submit" id="LetterGuessed" class=btn1 name="LetterGuessed" value="'.$letter.'">';
-        echo "&nbsp &nbsp &nbsp";
+        echo "<form name='check' method=POST><input type='hidden' name='end' value='0'></form>";
+        $_POST['streak']=0;
 
-  }
-echo "<br><br><br><br>";
-  for($x=0;$x<10;$x++)
-  {
-      echo "<input type='submit' name='LetterGuessed' class=btn1 value='".$x."'>"." &nbsp &nbsp	";
-  }
-echo "</div>";
-}
-
-if(isset($_SESSION['movie']))
-POSTmovie();
-setmovie();
-bolly();
-guessmovie();
-echo "<br>";
-if(!isset($_SESSION['lastletter']))
-{
-  $_SESSION['lastletter']="";
-}
-if(isset($_POST['LetterGuessed'])&&isset($_SESSION['lastletter']))
-
-{
-  if($_SESSION['lastletter']==$_POST['LetterGuessed'])
-  {
-
-    $pageRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) &&($_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0' ||  $_SERVER['HTTP_CACHE_CONTROL'] == 'no-cache');
-    if($pageRefreshed == 1)
-    {
-      // header('Location:http://localhost/files/git/uniquerandom.php?mark=0&Start=Start');
-      $pageRefreshed=0;
-    }
-
-    if(isset($_SESSION['guess1']))
-    {
-      if(isset($_POST['LetterGuessed']))
-      {
-          oldLetter();
-       }
-      echo "<div class='guess'>";
-      for($i=0;$i<strlen($_SESSION['guess1']);$i++)
-      {
-        echo $_SESSION['guess1'][$i]." ";
       }
-      echo " </div></font>";
-    }
+        $_POST['LetterGuessed'] = array();
+        echo "</div></font>";
   }
-  else
-  {
-    checkguessmovie(0);
-  }
-}
-else
-{
-  if(isset($_SESSION['guess1']))
+  function checkguessmovie($x)
   {
     if(isset($_POST['LetterGuessed']))
     {
         oldLetter();
     }
-    echo "<div class='guess'><div class='guesstext'>Guess the below movie: </div><br>";
-    for($i=0;$i<strlen($_SESSION['guess1']);$i++)
-    {
-      echo $_SESSION['guess1'][$i]."";
-    }
-    echo " </div></font>";
-
+              $mark=1;
+              if(isset($_SESSION['guess1']))
+              {
+                echo "<div class='guess'><font size=15>";
+                    for($i=0;$i<strlen($_SESSION['guess1']);$i++)
+                  {
+                      if(isset($_POST['LetterGuessed']))
+                        {
+                        $LetterGuessed=$_POST['LetterGuessed'];
+                            if($_SESSION['movie'][$i]==$LetterGuessed)
+                              {
+                                  $_SESSION['guess1'][$i]=$LetterGuessed;
+                                  $mark=0;
+                              }
+                        }
+                    echo $_SESSION['guess1'][$i]." ";
+                }
+                 echo "</div></font>";
+              }
+              if($x==0)
+              {
+                  if($mark==1&&isset($_SESSION['count']))
+                  {
+                  $_SESSION['count']--;
+                  }
+              }
   }
-}
-letters();
-checkwinlose();
+  function bolly()
+  {
 
-if(isset($_POST['LetterGuessed']))
-{
-$_SESSION['lastletter']=$_POST['LetterGuessed'];
-}
-echo "<br>
- <input type='submit' name='change' class=btn2 value='Change'>
- </div>";
+    $text="BOLLYWOOD";
+    echo "<br>";
+    echo "<div class='bolly'><font size=20>";
+    if(isset($_SESSION['count']))
+    for($i=0;$i<9-$_SESSION['count'];$i++)
+    {
+      echo $text[$i];
+    }
+    echo "</div></font>";
+  echo "<br>";
+  }
+  function letters()
+  {
+    echo "<div id=letters>";
+    for($x=0;$x<26;$x++)
+    {
+          if($x%6==0)
+          {
+            echo"<br><br>";
+          }
+          $letter=chr($x+65);
+          echo '<input type="submit" id="LetterGuessed"  class="btn1" name="LetterGuessed" value="'.$letter.'">';
+          echo "&nbsp &nbsp &nbsp";
 
- ?>
+    }
+  echo "<br><br>";
+    for($x=0;$x<10;$x++)
+    {
+        echo "<input type='submit' name='LetterGuessed'  class='btn1'  value='".$x."'>"." &nbsp &nbsp	";
+    }
+  echo "</div>";
+  }
+
+  if(isset($_SESSION['movie']))
+  GETmovie();
+  setmovie();
+  guessmovie();
+  echo "<br><Br>";
+  if(!isset($_SESSION['lastletter']))
+  {
+    $_SESSION['lastletter']="";
+  }
+  if(isset($_POST['LetterGuessed'])&&isset($_SESSION['lastletter']))
+
+  {
+    if($_SESSION['lastletter']==$_POST['LetterGuessed'])
+    {
+
+      $pageRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) &&($_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0' ||  $_SERVER['HTTP_CACHE_CONTROL'] == 'no-cache');
+      if($pageRefreshed == 1)
+      {
+        // header('Location:http://localhost/files/git/uniquerandom.php?mark=0&Start=Start');
+        $pageRefreshed=0;
+      }
+
+      if(isset($_SESSION['guess1']))
+      {
+        if(isset($_POST['LetterGuessed']))
+        {
+            oldLetter();
+         }
+        echo "<div class='guess'><font size=15>";
+        for($i=0;$i<strlen($_SESSION['guess1']);$i++)
+        {
+          echo $_SESSION['guess1'][$i]." ";
+        }
+        echo " </div></font>";
+      }
+    }
+    else
+    {
+      checkguessmovie(0);
+    }
+  }
+  else
+  {
+    if(isset($_SESSION['guess1']))
+    {
+      if(isset($_POST['LetterGuessed']))
+      {
+          oldLetter();
+      }
+      echo "<div class='guess'><font size=15>";
+      for($i=0;$i<strlen($_SESSION['guess1']);$i++)
+      {
+        echo $_SESSION['guess1'][$i]." ";
+      }
+      echo " </div></font>";
+
+    }
+  }
+  checkwinlose();
+bolly();
+  letters();
+
+  if(isset($_POST['streak']))
+  {
+  }
+  if(isset($_POST['LetterGuessed']))
+  {
+  $_SESSION['lastletter']=$_POST['LetterGuessed'];
+  }
+  echo "<br>
+   <input type='submit' name='change' class='btn2' value='Change'>
+  </center>";
+
+   ?>
 </div>
 </body>
 </html>
